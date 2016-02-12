ca49794597b5123ddd4172825fd58a78b4cc8d78
Paul Tremberth <paul.tremberth@gmail.com>
2016-02-12 18:54:28 +0100

    Add Python 3 compatibility + Python 3 Travis-CI tests

--- scrapyjs/utils.py
+++ scrapyjs/utils.py
@@ -1,32 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 import hashlib
+import six
+
+try:
+    from scrapy.utils.python import to_bytes
+except ImportError:
+    from scrapy.utils.python import unicode_to_str as to_bytes
 
 
 def dict_hash(obj, start=''):
     """ Return a hash for a dict, based on its contents """
-    h = hashlib.sha1(start)
-    h.update(obj.__class__.__name__)
+    h = hashlib.sha1(to_bytes(start))
+    h.update(to_bytes(obj.__class__.__name__))
     if isinstance(obj, dict):
         for key, value in sorted(obj.items()):
-            h.update(key)
-            h.update(dict_hash(value))
+            h.update(to_bytes(key))
+            h.update(to_bytes(dict_hash(value)))
     elif isinstance(obj, (list, tuple)):
         for el in obj:
-            h.update(dict_hash(el))
+            h.update(to_bytes(dict_hash(el)))
     else:
         # basic types
-        if isinstance(obj, (bool, int)):
+        if isinstance(obj, bool):
             value = str(int(obj))
-        elif isinstance(obj, (long, float)):
+        elif isinstance(obj, (six.integer_types, float)):
             value = str(obj)
-        elif isinstance(obj, unicode):
-            value = obj.encode('utf8')
-        elif isinstance(obj, bytes):
+        elif isinstance(obj, (six.text_type, bytes)):
             value = obj
         else:
             raise ValueError("Unsupported value type: %s" % obj.__class__)
-        h.update(value)
+        h.update(to_bytes(value))
     return h.hexdigest()
 
 
