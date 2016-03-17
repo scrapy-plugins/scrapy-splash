9a740fb0240155b3806b6ac669380b428baaa339
Mikhail Korobov <kmike84@gmail.com>
2016-03-17 18:09:48 +0600

    Revert "Update headers instead of overwriting them"

--- scrapyjs/middleware.py
+++ scrapyjs/middleware.py
@@ -100,13 +100,14 @@ class SplashMiddleware(object):
         splash_base_url = splash_options.get('splash_url', self.splash_base_url)
         splash_url = urljoin(splash_base_url, endpoint)
 
-        new_headers = request.headers.copy()
-        new_headers.update({'Content-Type': 'application/json'})
         req_rep = request.replace(
             url=splash_url,
             method='POST',
             body=body,
-            headers=new_headers,
+
+            # FIXME: original HTTP headers (including cookies)
+            # are not respected.
+            headers=Headers({'Content-Type': 'application/json'}),
         )
 
         self.crawler.stats.inc_value('splash/%s/request_count' % endpoint)
