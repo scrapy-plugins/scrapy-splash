637595bc697b9ffafc2ca6eca25e5b1e4380e525
Mikhail Korobov <kmike84@gmail.com>
2016-03-30 00:46:35 +0500

    expose SplashRequest as scrapyjs.SplashRequest

--- scrapyjs/__init__.py
+++ scrapyjs/__init__.py
@@ -5,3 +5,4 @@ from .middleware import SplashMiddleware, SlotPolicy
 from .dupefilter import SplashAwareDupeFilter, splash_request_fingerprint
 from .cache import SplashAwareFSCacheStorage
 from .response import SplashResponse, SplashTextResponse, SplashJsonResponse
+from .request import SplashRequest
