58fea6c9230226a7b773d9664d278b7aeeb6f843
Jean-Christophe Pelletier <pelletierj97@gmail.com>
2025-01-05 01:48:47 -0500

    Port request_fingerprint function back in scrapy_splash
    This is done for backward compatibility purposes, since we want the same
    behavior as the function deleted in Scrapy 2.12.0.

--- scrapy_splash/cache.py
+++ scrapy_splash/cache.py
@@ -7,12 +7,14 @@ See https://github.com/scrapy/scrapy/issues/900 for more info.
 """
 from __future__ import absolute_import
 import os
+from warnings import deprecated
 
 from scrapy.extensions.httpcache import FilesystemCacheStorage
 
 from .dupefilter import splash_request_fingerprint
 
 
+@deprecated("Use the REQUEST_FINGERPRINTER_CLASS Scrapy setting instead")
 class SplashAwareFSCacheStorage(FilesystemCacheStorage):
     def _get_request_path(self, spider, request):
         key = splash_request_fingerprint(request)
--- scrapy_splash/dupefilter.py
+++ scrapy_splash/dupefilter.py
@@ -5,15 +5,90 @@ See https://github.com/scrapy/scrapy/issues/900 for more info.
 """
 from __future__ import absolute_import
 from copy import deepcopy
+import hashlib
+from weakref import WeakKeyDictionary
+from warnings import deprecated
 
 from scrapy.dupefilters import RFPDupeFilter
 
+from scrapy.utils.python import to_bytes
 from scrapy.utils.url import canonicalize_url
-from scrapy.utils.request import request_fingerprint
 
 from .utils import dict_hash
 
 
+_deprecated_fingerprint_cache = WeakKeyDictionary()
+
+
+def _serialize_headers(
+    headers, request
+):
+    for header in headers:
+        if header in request.headers:
+            yield header
+            for value in request.headers.getlist(header):
+                yield value
+
+
+# From https://docs.scrapy.org/en/2.11/_modules/scrapy/utils/request.html
+# Needs to be added here since it was deletedin Scrapy 2.12
+def request_fingerprint(
+    request,
+    include_headers=None,
+    keep_fragments=False,
+):
+    """
+    Return the request fingerprint as an hexadecimal string.
+
+    The request fingerprint is a hash that uniquely identifies the resource the
+    request points to. For example, take the following two urls:
+
+    http://www.example.com/query?id=111&cat=222
+    http://www.example.com/query?cat=222&id=111
+
+    Even though those are two different URLs both point to the same resource
+    and are equivalent (i.e. they should return the same response).
+
+    Another example are cookies used to store session ids. Suppose the
+    following page is only accessible to authenticated users:
+
+    http://www.example.com/members/offers.html
+
+    Lots of sites use a cookie to store the session id, which adds a random
+    component to the HTTP Request and thus should be ignored when calculating
+    the fingerprint.
+
+    For this reason, request headers are ignored by default when calculating
+    the fingerprint. If you want to include specific headers use the
+    include_headers argument, which is a list of Request headers to include.
+
+    Also, servers usually ignore fragments in urls when handling requests,
+    so they are also ignored by default when calculating the fingerprint.
+    If you want to include them, set the keep_fragments argument to True
+    (for instance when handling requests with a headless browser).
+    """
+    processed_include_headers = None
+    if include_headers:
+        processed_include_headers = tuple(
+            to_bytes(h.lower()) for h in sorted(include_headers)
+        )
+    cache = _deprecated_fingerprint_cache.setdefault(request, {})
+    cache_key = (processed_include_headers, keep_fragments)
+    if cache_key not in cache:
+        fp = hashlib.sha1()
+        fp.update(to_bytes(request.method))
+        fp.update(
+            to_bytes(canonicalize_url(request.url, keep_fragments=keep_fragments))
+        )
+        fp.update(request.body or b"")
+        if processed_include_headers:
+            for part in _serialize_headers(processed_include_headers, request):
+                fp.update(part)
+        cache[cache_key] = fp.hexdigest()
+    return cache[cache_key]
+
+
+@deprecated("Use the REQUEST_FINGERPRINTER_CLASS Scrapy setting instead")
 def splash_request_fingerprint(request, include_headers=None):
     """ Request fingerprint which takes 'splash' meta key into account """
 
@@ -30,6 +105,7 @@ def splash_request_fingerprint(request, include_headers=None):
     return dict_hash(splash_options, fp)
 
 
+@deprecated("Use the REQUEST_FINGERPRINTER_CLASS Scrapy setting instead")
 class SplashAwareDupeFilter(RFPDupeFilter):
     """
     DupeFilter that takes 'splash' meta key in account.
